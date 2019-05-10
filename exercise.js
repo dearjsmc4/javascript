4fe1856a9985d6447976c692d2ce14dba14ec322
dearjsmc4 <dearjsmc4@gmail.com>
2019-05-10 12:46:21 +0900

    제어문 연습문제 풀이

--- exercise.js
+++ exercise.js
@@ -1,49 +1,49 @@
 
-1
+// 1
 var x = 15;
 if ( x > 10 && x < 20 ) {
     console.log(x);
 }
 
-2
+// 2
 for ( var i = 0 ; i < 10 ; i+=2 ) {
     console.log(i);
 }
 
-3
+// 3
 var even = '';
 for ( var i = 0 ; i < 10 ; i+=2 ) {
     even = even + i;
 }
 console.log(even);
 
-4
+// 4
 for ( var i = 9 ; i > 0 ; i-=2 ) {
     console.log(i);
 }
 
-5 
+//5 
 var i = 0;
 while(i < 10){
     console.log(i);
     i += 2;
 }
 
-6
+// 6
 var i = 9;
 while(i > 0){
     console.log(i);
     i -= 2;
 }
 
-7 
+// 7 
 var a = 0;
 for (var i = 0; i < 10; i++) {
     a += i;
 }
 console.log(a);
 
-8
+//8
 var a = 0;
 for ( var i = 0; i < 20; i++ ) {
     if ( i % 2 !== 0 && i % 3 !== 0 ) {
@@ -52,7 +52,7 @@ for ( var i = 0; i < 20; i++ ) {
 }
 console.log(a);
 
-9
+// 9
 var a = 0;
 for ( var i = 0; i < 20; i++ ) {
     if ( i % 2 === 0 || i % 3 === 0 ) {
@@ -61,7 +61,7 @@ for ( var i = 0; i < 20; i++ ) {
 }
 console.log(a);
 
-10
+// 10
 for ( var i = 1; i <= 6; i++ ) {
     for ( var a = 1; a <= 6; a++ ) {
         if ( a + i === 6 ) {
@@ -70,7 +70,7 @@ for ( var i = 1; i <= 6; i++ ) {
     }
 }
 
-11
+// 11
 var line = '';
 for ( var i = 1; i <= 5; i++ ) {
     for ( var a = 0; a < i; a++ ) {
@@ -80,7 +80,7 @@ for ( var i = 1; i <= 5; i++ ) {
 }
 console.log(line);
 
-12
+// 12
 var line = '';
 for ( var i = 5; i >= 1; i-- ) {
     for ( var s = 5; s > i; s--) {
@@ -93,7 +93,7 @@ for ( var i = 5; i >= 1; i-- ) {
 }
 console.log(line);
 
-13
+// 13
 var line = '';
 for ( var i = 5; i >= 1; i-- ) {
     for ( var a = 0; a < i; a++ ) {
@@ -103,7 +103,7 @@ for ( var i = 5; i >= 1; i-- ) {
 }
 console.log(line);
 
-14
+// 14
 var line = '';
 for ( var i = 1; i <= 5; i++ ) {
     for ( var s = 5; s > i; s--) {
@@ -116,7 +116,7 @@ for ( var i = 1; i <= 5; i++ ) {
 }
 console.log(line);
 
-15
+// 15
 var line = '';
 for ( var i = 1; i <= 5; i++ ) {
     for ( var s = 5; s > i; s--) {
@@ -129,7 +129,7 @@ for ( var i = 1; i <= 5; i++ ) {
 }
 console.log(line);
 
-16
+// 16
 var line = '';
 for ( var i = 5; i >= 1; i-- ) {
     for ( var s = 5; s > i; s--) {
