5bf4202bd65759023e71a7d2ed4a2924a1df3861
dearjsmc4 <dearjsmc4@gmail.com>
2019-05-16 10:22:56 +0900

    undefined 일때 수정

--- 0515/0515-2.js
+++ 0515/0515-2.js
@@ -1,10 +1,13 @@
 function alphaString46(s) {
   // eslint-disable-next-line no-restricted-globals
-  if (s.length > 3 && s.length < 7 && !isNaN(s)) {
-    return true;
-  }
-  return false;
+  const result = !isNaN(s * 1) && (s.length > 3 && s.length < 7) ? 'true' : 'false';
+  return result;
 }
 
-console.log(alphaString46('1234'));
-console.log(alphaString46('a123'));
+
+console.log(alphaString46('1234')); // true
+console.log(alphaString46('9014')); // true
+console.log(alphaString46('723'));  // false
+console.log(alphaString46('a234')); // false
+console.log(alphaString46(''));     // false
+console.log(alphaString46());       // false
