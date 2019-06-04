b7f9bb517310532182776d4afc9eeac2452e05ea
dearjsmc4 <dearjsmc4@gmail.com>
2019-06-04 10:51:29 +0900

    todo 3.0
    fetch 이용하기

---
+++ todo/js/app3.js
@@ -0,0 +1,131 @@
+/* eslint-disable func-names */
+(function () {
+  let todos = [];
+  const $todos = document.querySelector('.todos');
+  const $inputTodo = document.querySelector('.input-todo');
+  const $completeAll = document.querySelector('.complete-all');
+  const $clearCompleted = document.querySelector('.clear-completed > .btn');
+  const $nav = document.querySelector('.nav');
+  const $activeTodos = document.querySelector('.active-todos');
+  const $completedTodos = document.querySelector('.completed-todos');
+  let navId = 'all';
+
+  function render(resTodo) {
+    console.log(resTodo);
+    todos = resTodo;
+    let filterTodos = [];
+    if (navId === 'all') filterTodos = [...todos];
+    else if (navId === 'active') filterTodos = todos.filter(todo => !todo.completed);
+    else filterTodos = todos.filter(todo => todo.completed);
+
+    let html = '';
+    filterTodos.forEach(({ id, content, completed }) => {
+      html += `<li id="${id}" class="todo-item">
+      <input class="custom-checkbox" type="checkbox" id="ck-${id}" ${completed ? 'checked' : ''}>
+      <label for="ck-${id}">${content}</label>
+      <i class="remove-todo far fa-times-circle"></i>
+      </li>`;
+    });
+    $todos.innerHTML = html;
+    $activeTodos.innerHTML = todos.filter(todo => todo.completed !== true).length;
+    $completedTodos.innerHTML = todos.filter(todo => todo.completed === true).length;
+  }
+
+  window.onload = function getTodos() {
+    fetch('http://localhost:9000/todos')
+      .then(res => res.json())
+      .then(render)
+      .catch(console.log);
+  };
+
+  function generateId() {
+    return todos.length ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+  }
+
+  function removeTodo(id) {
+    fetch(`http://localhost:9000/todos/${id}`, {
+      method: 'DELETE'
+    })
+      .then(res => res.json())
+      .then(render)
+      .catch(console.log);
+  }
+
+  $todos.onchange = function (e) {
+    const id = +e.target.parentNode.id;
+    if (e.target.checked) {
+      e.target.setAttribute('checked', 'checked');
+    } else {
+      e.target.removeAttribute('checked');
+    }
+    fetch(`http://localhost:9000/todos/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ completed: !id.completed })
+    })
+      .then(res => res.json())
+      .then(render)
+      .catch(console.log);
+  };
+
+  $todos.onclick = function (e) {
+    if (e.target.classList.contains('remove-todo')) {
+      removeTodo(e.target.parentNode.id);
+    }
+  };
+
+  $inputTodo.onkeyup = function (e) {
+    const value = $inputTodo.value.trim();
+    if (value === '' || e.keyCode !== 13) return false;
+    fetch('http://localhost:9000/todos', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id: generateId(), content: value, completed: false })
+    })
+      .then(res => res.json())
+      .then(render)
+      .catch(console.log);
+    $inputTodo.value = '';
+  };
+
+  $completeAll.onchange = function (e) {
+    if (e.target.checked) {
+      fetch('http://localhost:9000/todos', {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ completed: true })
+      })
+        .then(res => res.json())
+        .then(render)
+        .catch(console.log);
+    } else {
+      fetch('http://localhost:9000/todos', {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ completed: false })
+      })
+        .then(res => res.json())
+        .then(render)
+        .catch(console.log);
+    }
+  };
+
+  $clearCompleted.onclick = function () {
+    fetch('http://localhost:9000/todos/completed', {
+      method: 'DELETE'
+    })
+      .then(res => res.json())
+      .then(render)
+      .catch(console.log);
+  };
+
+  $nav.onclick = function (e) {
+    [...$nav.children].forEach(item => item.classList.remove('active'));
+    e.target.className = 'active';
+    navId = e.target.id;
+    fetch('http://localhost:9000/todos')
+      .then(res => res.json())
+      .then(render)
+      .catch(console.log);
+  };
+}());
