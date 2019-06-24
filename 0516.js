87af13c7a7ddd0c18adc54f50136f2cdc0bd81d5
dearjsmc4 <dearjsmc4@gmail.com>
2019-06-24 13:07:32 +0900

    package.json name 변경

--- 0516.js
+++ 0516.js
@@ -1,18 +1,18 @@
 (function printNow() {
-  let today = new Date();
+  const today = new Date();
   const dayNames = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
 
-  let year = today.getFullYear();
-  let month = today.getMonth() + 1;
-  let day = today.getDate();
-  let dayName = dayNames[today.getDay()];
-  let hour = today.getHours();
-  let minutes = today.getMinutes();
-  let seconds = today.getSeconds();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+  const dayName = dayNames[today.getDay()];
+  const hour = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds();
 
-  let ampm = hour > 11 ? 'pm' : 'am';
+  const ampm = hour > 11 ? 'pm' : 'am';
 
-  let printDate = `${year}년 ${month}월 ${day}일 ${dayName} ${hour}:${minutes}:${seconds} ${ampm}`;
+  const printDate = `${year}년 ${month}월 ${day}일 ${dayName} ${hour}:${minutes}:${seconds} ${ampm}`;
   console.log(printDate);
   setTimeout(printNow, 1000);
 }());
